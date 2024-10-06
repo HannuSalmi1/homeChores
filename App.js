ad83411c2102d8c0ee3d6bd6c85c06cf3c953694
HannuSalmi1 <hannusalmi1@outlook.com>
2024-10-06 17:55:10 +0300

    start

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+
+
+
+const App = () => {
+ 
+  return (
+    <Text>HOME CHORES APP VERY GOOD</Text>
+    
+  );
+};
+
+export default App;
